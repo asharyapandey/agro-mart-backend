6d4ca803cc0ec65c43f45169ba659b4b60a049b1
asharyapandey <asharyap7@gmail.com>
2021-06-22 19:12:49 +0545

    Feature: Added local login

--- source/api/constants/user.constants.ts
+++ source/api/constants/user.constants.ts
@@ -1,4 +1,4 @@
 export const REGISTER_USER_ROUTE = "/user/register";
-export const LOCAL_LOGIN_ROUTE = "/user/register";
+export const LOCAL_LOGIN_ROUTE = "/user/login";
 export const GOOGLE_LOGIN_ROUTE = "/auth/google";
 export const GOOGLE_AUTH_CALLBACK_ROUTE = "/auth/google/callback";
--- source/api/routes/user.routes.ts
+++ source/api/routes/user.routes.ts
@@ -5,12 +5,30 @@ import {
     REGISTER_USER_ROUTE,
     GOOGLE_LOGIN_ROUTE,
     GOOGLE_AUTH_CALLBACK_ROUTE,
+    LOCAL_LOGIN_ROUTE,
 } from "../constants/user.constants";
 import { registerUser } from "../controllers/user.controllers";
+import { SUCCESS } from "../constants/status-codes.constants";
 
 const userRoutes = express.Router();
 
 userRoutes.post(REGISTER_USER_ROUTE, registerUser);
+
+userRoutes.post(
+    LOCAL_LOGIN_ROUTE,
+    (req: Request, res: Response, next: NextFunction) => {
+        passport.authenticate(
+            "local",
+            (err: Error, user: boolean, info: any) => {
+                if (info?.success) {
+                    res.status(SUCCESS).json(info);
+                } else {
+                    res.redirect("/login");
+                }
+            }
+        )(req, res, next);
+    }
+);
 
 userRoutes.get(
     GOOGLE_LOGIN_ROUTE,
